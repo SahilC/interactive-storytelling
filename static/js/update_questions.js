1b5558cad417d1cd51aa1e4e7081bee2524d9dd7
SahilC <sahilc.2200@gmail.com>
2017-06-22 20:09:07 +0530

    Updating code for SWAL

--- static/js/update_questions.js
+++ static/js/update_questions.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
                 contentType:'application/json',
                 data: JSON.stringify({file_name: 'data/FILE0573.MOV.txt'}),
                 success: function(response) {
+                    swal({
+                      title: 'Error!',
+                      text: 'Do you want to continue',
+                      type: 'error',
+                      confirmButtonText: 'Cool'
+                    });
                     var result = JSON.parse(response);
                     // console.log(result.stories)
                     responsiveVoice.speak(result['stories']['balahissardarwaza'][1]);
