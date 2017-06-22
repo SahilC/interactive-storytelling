063f4646aabca024fbf34614bf6384fcb6b15a2c
SahilC <sahilc.2200@gmail.com>
2017-06-22 23:58:20 +0530

    Updated Code for Q&A

--- static/js/update_questions.js
+++ static/js/update_questions.js
@@ -21,45 +21,54 @@ $(document).ready(function() {
 
 function check_and_wait(result) {
     var  start = new Date().getTime();
+    var pause = 0;
     var i = 0;
     var interval = setInterval(function() {
-            var time = (new Date().getTime() - start)/(1000.0);
+            var time = (new Date().getTime() - pause - start)/(1000.0);
+
             if(  time > result.final[i].time ) {
                 if(result.final[i].type == 'story') {
                     // responsiveVoice.speak(result.stories[result.final[i].name][0]);
                     console.log(result.final[i].name);
+                    i += 1;
                 } else {
-                    vid.pause()
-                    swal({
-                      title: "An input!",
-                      text: "Would you like to listen to a story about "+result.final[i].story.opt1 + " or " + result.final[i].story.opt2+"?" ,
-                      type: "input",
-                      showCancelButton: true,
-                      closeOnConfirm: false,
-                      animation: "slide-from-top",
-                      inputPlaceholder: "Enter choice here."
-                    },
-                    function(inputValue){
-                      if (inputValue === false) return false;
-                      
-                      if (inputValue === "") {
-                        swal.showInputError("You need to write something!");
-                        return false
-                      }
-                      console.log(result.final);
-                      if (result.final[i].story.opt1.includes(inputValue)) {
-                        vid.play();
-                        // responsiveVoice.speak(result.stories[result.final[i].story.m1][0]);
-                        console.log(result.final[i].story.m1);
-                        i += 1;
-                      } else {
-                        vid.play();
-                        // responsiveVoice.speak(result.stories[result.final[i].story.m2][0]);
-                        console.log(result.final[i].story.m2);
-                        i += 1;
-                      }
-                      // swal("Nice!", "You wrote: " + inputValue, "success");
-                    });
+                    if(vid.paused != true) {
+                        var pause_start = new Date().getTime();
+                        vid.pause()
+                        swal({
+                          title: "An input!",
+                          text: "Would you like to listen to a story about "+result.final[i].story.opt1 + " or " + result.final[i].story.opt2+"?" ,
+                          type: "input",
+                          showCancelButton: true,
+                          closeOnConfirm: false,
+                          animation: "slide-from-top",
+                          inputPlaceholder: "Enter choice here."
+                        },
+                        function(inputValue) {
+                          if (inputValue === false) return false;
+                          
+                          if (inputValue === "") {
+                            swal.showInputError("You need to write something!");
+                            return false
+                          }
+                          console.log(i);
+                          console.log(result.final[i].story.opt1);
+                          if (result.final[i].story.opt1.includes(inputValue)) {
+                            pause = pause + (new Date().getTime() - pause_start);
+                            vid.play();
+                            // responsiveVoice.speak(result.stories[result.final[i].story.m1][0]);
+                            console.log(result.final[i].story.m1);
+                            i += 1;
+                          } else {
+                            pause = pause + (new Date().getTime() - pause_start);
+                            vid.play();
+                            // responsiveVoice.speak(result.stories[result.final[i].story.m2][0]);
+                            console.log(result.final[i].story.m2);
+                            i += 1;
+                          }
+                          // swal("Nice!", "You wrote: " + inputValue, "success");
+                        });
+                    }
                 }
             } else { 
                 return false; 
